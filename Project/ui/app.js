0d067eb35cd3c3c7efdbae164608dc06a248a11e
Miles <128334837+Mitles1234@users.noreply.github.com>
2026-06-21 12:25:02 +1000

    21/06/2026 - Home
    Changed the settings panel to instead of opening a new HTML page, but just a Modal instead. Currently, it doesnt have any information in it, but I am likely going to add in a file directory selector to use for storing the project files. Also added an error message using the toast system, instead of a special error.html for when the printer cannot connect.

--- Project/ui/app.js
+++ Project/ui/app.js
@@ -13,9 +13,12 @@ function navigate(url, el) {
         return;
     }
 
-    fetch(url, { mode: 'no-cors', signal: AbortSignal.timeout(3000) })
+    fetch(url, { mode: 'no-cors', signal: AbortSignal.timeout(20) })
         .then(() => document.getElementById('content').src = url)
-        .catch(() => document.getElementById('content').src = 'error.html');
+        .catch(() => {
+            document.getElementById('content').src = 'equipment.html';
+            showToast('Failed to load page, Please check the 3D Printer Settings', true);
+        });
 }
 
 // Set pill to first link on startup
@@ -25,4 +28,28 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-// --- Python Backend ---
+
+function OpenSettingsModal() {
+    document.getElementById('settings-modal').classList.add('open');
+}
+ 
+function closeSettingsModal() {
+    document.getElementById('settings-modal').classList.remove('open');
+}
+ 
+function confirmSettingsModal() {
+    //For When I have Inputs that the user can set lol
+ 
+    closeSettingsModal();
+}
+ 
+// click outside the modal box to close it
+document.getElementById('settings-modal').addEventListener('click', (event) => {
+    if (event.target === document.getElementById('settings-modal')) {
+        closeSettingsModal();
+    }
+});
+ 
+window.OpenSettingsModal = OpenSettingsModal;
+window.closeSettingsModal = closeSettingsModal;
+window.confirmSettingsModal = confirmSettingsModal;
